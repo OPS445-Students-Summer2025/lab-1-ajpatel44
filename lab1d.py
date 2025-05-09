23919fdb9cf6714ab73ce028b492c48e9dca1dc0
Aryan Patel <ajpatel44@myseneca.ca>
2025-05-08 22:08:12 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This program performs a math operation using variables.'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+print(f'{x} + {y} * {z} = {result}')
